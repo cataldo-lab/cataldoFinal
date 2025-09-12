00346a731074dc1cf0338b740b35c3b44960e393
cataldo-lab <cataldobenjamin@gmail.com>
2025-09-11 22:53:51 -0300

    tabla representante

---
+++ backend/src/entity/respresentante.entity.js
@@ -0,0 +1,52 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+export const representanteSchema = new EntitySchema({
+    name: "Representante",
+    tableName: "representante",
+    columns: {
+        id_representante: {
+            type: "int",
+            primary: true,
+            generated: true,
+        },
+        nombre_representante: {
+            type: "varchar",
+            length: 100,
+            nullable: false
+        },
+        apellido_representante: {
+            type: "varchar",
+            length: 100,
+            nullable: false
+        },
+        rut_representante: {
+            type: "varchar",
+            length: 12,
+            nullable: false
+        },
+        cargo_representante: {
+            type: "varchar",
+            length: 100,
+            nullable: false
+        },
+        fono_representante: {
+            type: "varchar",
+            length: 15,
+            nullable: false
+        },
+        correo_representante: {
+            type: "varchar",
+            length: 100,
+            nullable: false
+        },
+        creado_en:{
+            type: "timestamp",
+            createDate: true,
+            nullable: false
+        }
+    },
+    relations: {
+        // Define any relationships with other entities here
+    }
+});
