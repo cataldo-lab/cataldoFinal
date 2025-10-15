3981cf2ef1ea2876e0cddf50de677375913df3fc
cataldo-lab <cataldobenjamin@gmail.com>
2025-10-15 12:15:06 -0300

    producto

--- backend/src/controllers/cliente/cliente.controller.js
+++ backend/src/controllers/cliente/cliente.controller.js
@@ -1,10 +1,13 @@
 "use strict";
 
+//Cliente puede ver su perfil
+
 export async function getPerfilCliente(req, res){
 
 }
 
 
+//Cliente puede ver sus pedidos
 
 export async function getMisPedidos(req, res){
     
