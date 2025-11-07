23883c76e7a70eba55b68ff463222dc8dedf32ec
cataldo-lab <cataldobenjamin@gmail.com>
2025-11-07 16:51:06 -0300

    nuevo

--- backend/src/services/gerente/gerente.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-import { AppDataSource } from "../../config/configDb.js";
-import Cliente from "../../entity/personas/cliente.entity.js";
-import { User } from "../../entity/personas/user.entity.js";
-import { Producto } from "../entity/producto.entity.js";
-import { Producto_Operacion } from "../entity/producto_operacion.entity.js";
-
-
