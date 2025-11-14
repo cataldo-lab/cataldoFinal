289f4512ef5bb0d24edb627d15edde75fa173a81
Sukumbia <85468340+cataldo-lab@users.noreply.github.com>
2025-11-14 12:34:40 -0300

    Revert "fix: Cambiar rutas de autenticación para evitar bloqueo por extension…"

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,10 +4,9 @@ import { login, logout, register } from "../controllers/auth.controller.js";
 
 const router = Router();
 
-// Rutas cambiadas para evitar bloqueo por extensiones de navegador
 router
-  .post("/verify", login)
-  .post("/create", register)
-  .post("/end", logout);
+  .post("/login", login)
+  .post("/register", register)
+  .post("/logout", logout);
 
 export default router;
